5c206fc574d397c47478f1f98c3bb1ec9677db06
Liane Guillou <lguillou@moser.inf.ed.ac.uk>
2017-09-29 15:18:42 +0100

    Added NER

--- pipeline/main.py
+++ pipeline/main.py
@@ -10,9 +10,13 @@ import sys
 import codecs
 import locale
 import glob
+import csv
 import ConfigParser
 import logging
+import subprocess
 from datetime import datetime
+from multiprocessing import Process
+from nltk.tag import StanfordNERTagger
 
 # Custom
 sys.path.insert(0,'/afs/inf.ed.ac.uk/user/l/lguillou/Tools/UnstableParser')
@@ -88,11 +92,6 @@ def unstable_parser(config):
     outputdir = home + '/' + outdir
     logging.info('Parsing input files in: ' + indir)
     parser.parse(outputdir, files)
-#    for infile in files:
-#        logging.info('  '+infile)
-#        outputfile = infile.split('/')[-1].split('.')[0]+'-unstable.conllu'
-#        parser.parse(outputdir, [infile])
-#        parser.parse(outputdir, outputfile, [infile])
         
 
 # Post-process the UnstableParser output (German only)
@@ -117,6 +116,101 @@ def post_process_parsed_output(config):
             postproc.restore(originalfile, infile, outdir)
 
 
+def pre_process_ner(config):
+    # Get home directory
+    home = config.get('General','home')
+    # Get output directory
+    outdir = config.get('GermaNER','pre_proc_out_dir')
+    # Get UDPipe-processed files
+    indir = config.get('UDPipe','out_dir')
+    files = glob.glob(home+'/'+indir+'/*')
+    for f in files:
+        tokens = []
+        with open(f, 'r') as infile:
+            for line in infile:
+                if line[0] != '#':
+                    if line == '\n':
+                        tokens.append('\n')
+                    else:
+                        tokens.append(line.split('\t')[1])
+        outfilename = home + '/' + outdir + '/' + f.split('/')[-1].split('.')[0] + '.tsv'
+        with open(outfilename, 'w') as outfile:
+            for tok in tokens:
+                if tok == '\n':
+                    outfile.write(tok)
+                else:
+                    outfile.write(tok+'\n')
+
+
+def GermaNER(config):
+    # Get home directory
+    home = config.get('General','home')
+    # Get jar file path
+    jar_file = config.get('GermaNER','jar_file')
+    # Get input and output directories, and input files
+    indir = config.get('GermaNER','pre_proc_out_dir')
+    outdir = config.get('GermaNER','out_dir')
+    files = glob.glob(home+'/'+indir+'/*.tsv')
+    for f in files:
+        outfile = home + '/' + outdir + '/' + f.split('/')[-1]
+        # Perform NER
+        subprocess.call(['java', '-jar', jar_file, '-t', f, '-o', outfile])
+
+
+def StanfordNER(config):
+    # Get home directory
+    home = config.get('General','home')
+    # Get jar file path
+    jar_file = config.get('StanfordNER','jar_file')
+    model_file = config.get('StanfordNER','model_file')
+    # Get input and output directories, and input files
+    indir = config.get('GermaNER','pre_proc_out_dir')
+    outdir = config.get('StanfordNER','out_dir')
+    files = glob.glob(home+'/'+indir+'/*.tsv')
+    # Initialise NER tagger
+    st = StanfordNERTagger(model_file, jar_file, encoding='utf-8')
+    for f in files:
+        raw_sentences = []
+        tagged_sentences = []
+        # Read tokenised raw sentences
+        with open(f, 'r') as infile:
+            tokens = []
+            for line in infile:
+                if line == '\n':
+                    raw_sentences.append(tokens)
+                    tokens = []
+                else:
+                    tokens.append(line.rstrip('\n'))
+        # Tag sentences
+        for sent in raw_sentences:
+            tagged_sent = st.tag(sent)
+            tagged_sentences.append(tagged_sent)
+        # Write tagged sentences to file
+        outfilename = home + '/' + outdir + '/' + f.split('/')[-1]
+        with codecs.open(outfilename, 'w', 'utf-8') as outfile:
+            for sent in tagged_sentences:
+                for tok in sent:
+                    outfile.write(tok[0]+'\t'+tok[1]+'\n')
+                outfile.write('\n')
+
+def parse(configmap):
+    print('process: parse')
+    # Dependency parse with the UnstableParser 
+    unstable_parser(configmap)
+    # Post-process UnstableParser output
+    post_process_parsed_output(configmap)
+
+
+def NER(configmap):
+    print('process: NER')
+    # Format file with one token per line (take tokenisation from UDPipe)
+    pre_process_ner(configmap)
+    # Apply NER using GermaNER
+#    GermaNER(configmap)
+    # Apply NER using Stanford NER
+    StanfordNER(configmap)
+
+
 if __name__ == "__main__":
     # Set up logging
     logging.basicConfig(filename='pipeline.log',level=logging.DEBUG)
@@ -125,11 +219,12 @@ if __name__ == "__main__":
     configfile = sys.argv[1]
     # Get configuration settings
     configmap = get_config(configfile)
-    # Pre-process files with Udpipe
+    # Pre-process files with UDPipe
     udpipe(configmap)
-    # Dependency parse with the UnstableParser
-    unstable_parser(configmap)
-    # Post-process UnstableParser output
-    post_process_parsed_output(configmap)
+    # Simultaneously parse and perform NER
+    parse_proc = Process(target=parse, args=(configmap,))
+#    parse_proc.start()
+    ner_proc = Process(target=NER, args=(configmap,))
+    ner_proc.start()
     # Exit
     logging.info('Finished at: '+str(datetime.now())+'\n\n')
