f58732da057adbb1ec3f45887bb01280f9d6394c
Liane Guillou <lguillou@moser.inf.ed.ac.uk>
2017-10-19 17:55:03 +0100

    Extracting binary relations: arcs from entity to predicate, handling compound verbs

--- pipeline/binary_relation.py
+++ pipeline/binary_relation.py
@@ -78,23 +78,38 @@ class BinaryRelation():
                 e = ent['sentences'][sent]['entities'][entity]
                 start = e['start']
                 offset = e['offset']
-#                nestring = e['namedEntity']
-#                print(sentstring)
-#                print(sent,entity,start,offset,nestring)
                 starttok = char_to_tok[start]
                 endtok = char_to_tok[start+offset-1]
                 ent['sentences'][sent]['entities'][entity]['starttok'] = starttok
                 ent['sentences'][sent]['entities'][entity]['endtok'] = endtok
-#                tokenrange = (starttok,endtok)
-#                print(tokenrange)
-#                print('---------------------')
                 # Get relations
                 dpsentence = dp[int(sent)]
                 dpsenttree = [dt[int(sent)]]
-                updatedentity = ent['sentences'][sent]['entities'][entity]
-                r = self.get_relations(dpsentence, dpsenttree, updatedentity)
+            updatedentities = ent['sentences'][sent]['entities']
+            r = self.get_relations(dpsentence, dpsenttree, updatedentities)
         return rels
 
+
+    def traverse_parse_tree(self, dt, e, deps, preds):
+        for node in dt:
+            # Get arcs pointing from the entity token
+            if node.data['id'] in e:
+                d = node.data
+                deps.append((d['id'], d['head'], d['deprel']))
+                # Maintain a dictionary of predicates to be used for recording compounds
+                if d['head'] not in preds:
+                    preds[d['head']] = {'compound': [], 'case': None}
+            # Record (compound) particle verbs (serial verbs do not appear to apply to German)    
+            elif node.data['head'] in preds and node.data['deprel'] == 'compound:prt':
+                preds[d['head']]['compound'].append(node.data['id'])
+            elif node.data['head'] in e and node.data['deprel'] == 'case':
+                pass ### CONTINUE ...
+            self.traverse_parse_tree(node.children, e, deps, preds)
+        return (deps, preds)
+
+
+    #[TreeNode(data=OrderedDict([('id', 2), ('form', 'wuchs'), ('lemma', 'wachsen'), ('upostag', 'VERB'), ('xpostag', 'VVFIN'), ('feats', None), ('head', 0), ('deprel', 'root'), ('deps', None), ('misc', None)]), children=[TreeNode(data=OrderedDict([('id', 1), ('form', 'Merkel'), ('lemma', 'Merkel'), ('upostag', 'NOUN'), ('xpostag', 'NN'), ('feats', None), ('head', 2), ('deprel', 'nsubj'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 5), ('form', 'DDR'), ('lemma', 'DDR'), ('upostag', 'PROPN'), ('xpostag', 'NE'), ('feats', None), ('head', 2), ('deprel', 'obl'), ('deps', None), ('misc', None)]), children=[TreeNode(data=OrderedDict([('id', 3), ('form', 'in'), ('lemma', 'in'), ('upostag', 'ADP'), ('xpostag', 'APPR'), ('feats', None), ('head', 5), ('deprel', 'case'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 4), ('form', 'der'), ('lemma', 'der'), ('upostag', 'DET'), ('xpostag', 'ART'), ('feats', None), ('head', 5), ('deprel', 'det'), ('deps', None), ('misc', None)]), children=[])]), TreeNode(data=OrderedDict([('id', 6), ('form', 'auf'), ('lemma', 'auf'), ('upostag', 'ADP'), ('xpostag', 'PTKVZ'), ('feats', None), ('head', 2), ('deprel', 'compound:prt'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 13), ('form', 't\xc3\xa4tig'), ('lemma', 't\xc3\xa4tig'), ('upostag', 'ADJ'), ('xpostag', 'ADJD'), ('feats', None), ('head', 2), ('deprel', 'conj'), ('deps', None), ('misc', None)]), children=[TreeNode(data=OrderedDict([('id', 7), ('form', 'und'), ('lemma', 'und'), ('upostag', 'CCONJ'), ('xpostag', 'KON'), ('feats', None), ('head', 13), ('deprel', 'cc'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 8), ('form', 'war'), ('lemma', 'sein'), ('upostag', 'VERB'), ('xpostag', 'VAFIN'), ('feats', None), ('head', 13), ('deprel', 'cop'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 9), ('form', 'dort'), ('lemma', 'dort'), ('upostag', 'ADV'), ('xpostag', 'ADV'), ('feats', None), ('head', 13), ('deprel', 'advmod'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 11), ('form', 'Physikerin'), ('lemma', 'Physikerin'), ('upostag', 'NOUN'), ('xpostag', 'NN'), ('feats', None), ('head', 13), ('deprel', 'nmod'), ('deps', None), ('misc', None)]), children=[TreeNode(data=OrderedDict([('id', 10), ('form', 'als'), ('lemma', 'als'), ('upostag', 'ADP'), ('xpostag', 'KOKOM'), ('feats', None), ('head', 11), ('deprel', 'case'), ('deps', None), ('misc', None)]), children=[])]), TreeNode(data=OrderedDict([('id', 12), ('form', 'wissenschaftlich'), ('lemma', 'wissenschaftlich'), ('upostag', 'ADJ'), ('xpostag', 'ADJD'), ('feats', None), ('head', 13), ('deprel', 'advmod'), ('deps', None), ('misc', None)]), children=[])]), TreeNode(data=OrderedDict([('id', 14), ('form', '.'), ('lemma', '.'), ('upostag', 'PUNCT'), ('xpostag', '$.'), ('feats', None), ('head', 2), ('deprel', 'punct'), ('deps', None), ('misc', None)]), children=[])])]
+    
 
     def get_relations(self, dp, dt, ent):
         ls = []
@@ -103,9 +118,26 @@ class BinaryRelation():
         ss = ' '.join(ls)
         print ss
         print ent
-        for root in dt:
-            print root.data
-            print root.children
+        print dt
+        print('...ENTITY SPANS...')
+        ent_spans = {}
+        # Won't handle overlapping entities - not sure if we'd ever see these though
+        for entity in ent:
+            for x in range(ent[entity]['starttok'],ent[entity]['endtok']+1):
+                ent_spans[x] = entity
+        print(ent_spans)
+        print('...TRAVERSAL...')
+        res = self.traverse_parse_tree(dt, ent_spans, [], {})
+        rel_deps = res[0]
+        preds = res[1]
+        print(rel_deps)
+        print(preds)
+        for rd in rel_deps:
+            pred = dp[rd[1]-1]['form'] # Replace with lemma?
+            if rd[1] in preds:
+                for element in preds[rd[1]]['compound']:
+                    pred += '_'+dp[element-1]['form'] # Change order and use lemma so that wuchs_auf -> aufwachsen?
+            print(dp[rd[0]-1]['form'], pred, rd[2])
         print('------')
 
 
@@ -113,25 +145,15 @@ class BinaryRelation():
 if __name__ == "__main__":
     # execute only if run as a script    
 
-    test_data = """
-    1       Merkel  Merkel  NOUN    NN      _       2       nsubj   _       _
-    2       wuchs   wachsen VERB    VVFIN   _       0       root    _       _
-    3       in      in      ADP     APPR    _       5       case    _       _
-    4       der     der     DET     ART     _       5       det     _       _
-    5       DDR     DDR     PROPN   NE      _       2       obl     _       _
-    6       auf     auf     ADP     PTKVZ   _       2       compound:prt    _       _
-    7       und     und     CCONJ   KON     _       13      cc      _       _
-    8       war     sein    VERB    VAFIN   _       13      cop     _       _
-    9       dort    dort    ADV     ADV     _       13      advmod  _       _
-    10      als     als     ADP     KOKOM   _       11      case    _       _
-    11      Physikerin      Physikerin      NOUN    NN      _       13      nmod    _       _
-    12      wissenschaftlich        wissenschaftlich        ADJ     ADJD    _       13      advmod  _       _
-    13      tätig   tätig   ADJ     ADJD    _       2       conj    _       _
-    14      .       .       PUNCT   $.      _       2       punct   _       _
+    configfile = 'config.ini'
+    cfg = ConfigParser.ConfigParser()
+    cfg.read(configfile)
+    bin_rel = BinaryRelation(cfg)
     
-    """
+    p = bin_rel.read_dependency_parse('test.conllu')
+    dparse = p[0]
+    dtree = p[1]
+    
+    test_entities = {"file": "/afs/inf.ed.ac.uk/user/l/lguillou/Code/question-answering/pipeline/03a-ner-output/de-input-1.tsv", "sentences": {"0": {"entities": {"0": {"start": 20, "disambiguatedURL": "http://de.dbpedia.org/resource/Deutsche_Demokratische_Republik", "namedEntity": "DDR", "FIGERType": "/location/country", "offset": 3}, "1": {"start": 0, "disambiguatedURL": "http://de.dbpedia.org/resource/Angela_Merkel", "namedEntity": "Merkel", "FIGERType": "/person/politician", "offset": 6}}, "sentenceStr": "<entity>Merkel</entity> wuchs in der <entity>DDR</entity> auf und war dort als Physikerin wissenschaftlich tätig ."}}}
 
-    test_entities = {"2": {"entities": {"0": {"start": 20, "disambiguatedURL": "http://de.dbpedia.org/resource/Deutsche_Demokratische_Republik", "namedEntity": "DDR", "FIGERType": "/location/country", "offset": 3}, "1": {"start": 0, "disambiguatedURL": "http://de.dbpedia.org/resource/Angela_Merkel", "namedEntity": "Merkel", "FIGERType": "/person/politician", "offset": 6}}, "sentenceStr": "<entity>Merkel</entity> wuchs in der <entity>DDR</entity> auf und war dort als Physikerin wissenschaftlich tätig ."}}
-
-    dparse = parse_conllu(data)
-    dtree = parse_conllu_tree(data)
+    bin_rel.extract(dparse,dtree,test_entities)
