aecb81d152d31e95d91ae6b25db85858c852437f
Liane Guillou <lguillou@moser.inf.ed.ac.uk>
2017-10-20 11:29:07 +0100

    Added case relation

--- pipeline/binary_relation.py
+++ pipeline/binary_relation.py
@@ -92,24 +92,24 @@ class BinaryRelation():
 
     def traverse_parse_tree(self, dt, e, deps, preds):
         for node in dt:
-            # Get arcs pointing from the entity token
-            if node.data['id'] in e:
-                d = node.data
+            d = node.data
+            # Get arcs pointing from the entity token (outgoing)
+            if d['id'] in e:
                 deps.append((d['id'], d['head'], d['deprel']))
                 # Maintain a dictionary of predicates to be used for recording compounds
                 if d['head'] not in preds:
                     preds[d['head']] = {'compound': [], 'case': None}
+            # Get arcs pointing from the token to the entity (incoming)
+            elif d['head'] in e and d['deprel'] == 'case': # Case only for now
+                head_chain = [item for item in deps if item[0] == d['head']]
+                if head_chain[0][1] in preds:
+                    preds[head_chain[0][1]]['case'] = d['id']
             # Record (compound) particle verbs (serial verbs do not appear to apply to German)    
-            elif node.data['head'] in preds and node.data['deprel'] == 'compound:prt':
-                preds[d['head']]['compound'].append(node.data['id'])
-            elif node.data['head'] in e and node.data['deprel'] == 'case':
-                pass ### CONTINUE ...
+            elif d['head'] in preds and d['deprel'] == 'compound:prt':
+                preds[d['head']]['compound'].append(d['id'])
             self.traverse_parse_tree(node.children, e, deps, preds)
         return (deps, preds)
 
-
-    #[TreeNode(data=OrderedDict([('id', 2), ('form', 'wuchs'), ('lemma', 'wachsen'), ('upostag', 'VERB'), ('xpostag', 'VVFIN'), ('feats', None), ('head', 0), ('deprel', 'root'), ('deps', None), ('misc', None)]), children=[TreeNode(data=OrderedDict([('id', 1), ('form', 'Merkel'), ('lemma', 'Merkel'), ('upostag', 'NOUN'), ('xpostag', 'NN'), ('feats', None), ('head', 2), ('deprel', 'nsubj'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 5), ('form', 'DDR'), ('lemma', 'DDR'), ('upostag', 'PROPN'), ('xpostag', 'NE'), ('feats', None), ('head', 2), ('deprel', 'obl'), ('deps', None), ('misc', None)]), children=[TreeNode(data=OrderedDict([('id', 3), ('form', 'in'), ('lemma', 'in'), ('upostag', 'ADP'), ('xpostag', 'APPR'), ('feats', None), ('head', 5), ('deprel', 'case'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 4), ('form', 'der'), ('lemma', 'der'), ('upostag', 'DET'), ('xpostag', 'ART'), ('feats', None), ('head', 5), ('deprel', 'det'), ('deps', None), ('misc', None)]), children=[])]), TreeNode(data=OrderedDict([('id', 6), ('form', 'auf'), ('lemma', 'auf'), ('upostag', 'ADP'), ('xpostag', 'PTKVZ'), ('feats', None), ('head', 2), ('deprel', 'compound:prt'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 13), ('form', 't\xc3\xa4tig'), ('lemma', 't\xc3\xa4tig'), ('upostag', 'ADJ'), ('xpostag', 'ADJD'), ('feats', None), ('head', 2), ('deprel', 'conj'), ('deps', None), ('misc', None)]), children=[TreeNode(data=OrderedDict([('id', 7), ('form', 'und'), ('lemma', 'und'), ('upostag', 'CCONJ'), ('xpostag', 'KON'), ('feats', None), ('head', 13), ('deprel', 'cc'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 8), ('form', 'war'), ('lemma', 'sein'), ('upostag', 'VERB'), ('xpostag', 'VAFIN'), ('feats', None), ('head', 13), ('deprel', 'cop'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 9), ('form', 'dort'), ('lemma', 'dort'), ('upostag', 'ADV'), ('xpostag', 'ADV'), ('feats', None), ('head', 13), ('deprel', 'advmod'), ('deps', None), ('misc', None)]), children=[]), TreeNode(data=OrderedDict([('id', 11), ('form', 'Physikerin'), ('lemma', 'Physikerin'), ('upostag', 'NOUN'), ('xpostag', 'NN'), ('feats', None), ('head', 13), ('deprel', 'nmod'), ('deps', None), ('misc', None)]), children=[TreeNode(data=OrderedDict([('id', 10), ('form', 'als'), ('lemma', 'als'), ('upostag', 'ADP'), ('xpostag', 'KOKOM'), ('feats', None), ('head', 11), ('deprel', 'case'), ('deps', None), ('misc', None)]), children=[])]), TreeNode(data=OrderedDict([('id', 12), ('form', 'wissenschaftlich'), ('lemma', 'wissenschaftlich'), ('upostag', 'ADJ'), ('xpostag', 'ADJD'), ('feats', None), ('head', 13), ('deprel', 'advmod'), ('deps', None), ('misc', None)]), children=[])]), TreeNode(data=OrderedDict([('id', 14), ('form', '.'), ('lemma', '.'), ('upostag', 'PUNCT'), ('xpostag', '$.'), ('feats', None), ('head', 2), ('deprel', 'punct'), ('deps', None), ('misc', None)]), children=[])])]
-    
 
     def get_relations(self, dp, dt, ent):
         ls = []
@@ -137,6 +137,11 @@ class BinaryRelation():
             if rd[1] in preds:
                 for element in preds[rd[1]]['compound']:
                     pred += '_'+dp[element-1]['form'] # Change order and use lemma so that wuchs_auf -> aufwachsen?
+                if preds[rd[1]]['case']:
+                    case = dp[preds[rd[1]]['case']-1]['form']
+                else:
+                    case = ''
+                pred += ':' + case
             print(dp[rd[0]-1]['form'], pred, rd[2])
         print('------')
 
