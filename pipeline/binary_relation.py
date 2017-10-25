13697d07025d1394ba23d5995e5434c508dee264
Liane Guillou <lguillou@moser.inf.ed.ac.uk>
2017-10-25 14:43:04 +0100

    Format binary relations for output. Output format should match that of the English pipeline

--- pipeline/binary_relation.py
+++ pipeline/binary_relation.py
@@ -35,7 +35,9 @@ class BinaryRelation():
             entities = self.read_entities(ef)
             # Extract binary relations
             relations = self.extract(dtree, entities)
-
+            # Write to file
+            self.write_to_file(relations)
+            
 
     # Read the dependency parser output
     def read_dependency_parse(self, filename):
@@ -66,21 +68,23 @@ class BinaryRelation():
     # Convert character offset to token offset
     def convert_offsets(self,sentence):
         conv = {}
-        sent = sentence.replace('<entity>','').replace('</entity>','')
+#        sent = sentence.replace('<entity>','').replace('</entity>','')
         counter = 1
-        for x in range(0,len(sent)):
+        for x in range(0,len(sentence)):
             conv[x] = counter
-            if sent[x] == ' ':
+            if sentence[x] == ' ':
                 counter += 1
         return conv
     
     
     # Perform the extraction
     def extract(self, dt, ent):
-        rels = []
+        rels = {}
         # Find start and end tokens for entities from character start and offset
         for sent in ent['sentences']:
-            sentstring = ent['sentences'][sent]['sentenceStr']
+            dpsenttree = dt[int(sent)]
+            sentstring = self.get_sentence(dpsenttree)
+            print sentstring
             char_to_tok = self.convert_offsets(sentstring)
             for entity in ent['sentences'][sent]['entities']:
                 e = ent['sentences'][sent]['entities'][entity]
@@ -90,13 +94,15 @@ class BinaryRelation():
                 endtok = char_to_tok[start+offset-1]
                 ent['sentences'][sent]['entities'][entity]['starttok'] = starttok
                 ent['sentences'][sent]['entities'][entity]['endtok'] = endtok
-                # Get relations
-                dpsenttree = dt[int(sent)]
             updatedentities = ent['sentences'][sent]['entities']
+            # Get relations
             r = self.get_relations(dpsenttree, updatedentities)
+            rels[sent] = {'sentence': sentstring, 'relations': r}
+            print(rels)
         return rels
 
 
+    # Extract the sentence text from the dependency tree
     def get_sentence(self, dt):
         t = []
         for node_index in dt.nodes:
@@ -107,11 +113,11 @@ class BinaryRelation():
         return s
     
 
+    # Find the binary relations
     def get_relations(self, dt, ent):
-        sentence = self.get_sentence(dt)
         print dt
-        print sentence
         print('...ENTITY SPANS...')
+        # Identify entity spans (just get the heads)
         ent_span_heads = {}
         # Won't handle overlapping entities - not sure if we'd ever see these though
         for entity in ent:
@@ -124,11 +130,14 @@ class BinaryRelation():
                 del ent_span_heads[i]
         print(ent_span_heads)
         print('...TRAVERSAL...')
+        # Traverse the tree and extract information
         res = self.traverse_parse_tree(dt, ent_span_heads, 0, [], {})
         rel_deps = res[0]
         preds = res[1]
         print(rel_deps)
         print(preds)
+        # Build relations
+        r = []
         for rd in rel_deps:
             entity_number = ent_span_heads[rd[0]]
             entity_string = ent[entity_number]['namedEntity'].replace(' ','_')
@@ -140,9 +149,40 @@ class BinaryRelation():
                     case = dt.nodes[preds[rd[1]]['case']]['word']
                     pred += ':' + case
             print(entity_string, pred, rd[2])
-        print('------') 
+            r.append((entity_number, pred))
+        print('------')
+        formatted = self.format_relations(r, ent)
+        return formatted
+
+
+    def format_relations(self, rels, ents):
+        result = []
+        d = {}
+        for r in rels:
+            if r[1] in d and r[0] not in d[r[1]]:
+                d[r[1]].append(r[0])
+            else:
+                d[r[1]] = [r[0]]
+        for pred in d:
+            if pred != 'none' and len(d[pred]) == 2: # Binary relation
+                # Sort the arguments of the predicates
+                args = []
+                for arg in d[pred]:
+                    args.append((arg,ents[arg]['starttok']))
+                l = [y[0] for y in sorted(args, key=lambda x: x[1])]
+                # Format the predicate string
+                p = pred.replace(':','.')
+                # Construct the relation string to match the format from the English pipeline
+                s = '('+p+'.1,'+p+'.2)'
+                s += '#none' if ents[l[0]]['FIGERType'] == 'none' else '#'+ents[l[0]]['FIGERType'].split('/')[1]
+                s += '#none' if ents[l[1]]['FIGERType'] == 'none' else '#'+ents[l[1]]['FIGERType'].split('/')[1]
+                s += '::'+ents[l[0]]['namedEntity']
+                s += '::'+ents[l[1]]['namedEntity']
+                result.append(s)
+        return result
         
 
+    # Traverse the parse tree
     def traverse_parse_tree(self, dt, e, node_index, rels, preds):
         children = sorted(chain.from_iterable(dt.nodes[node_index]['deps'].values()))
         for child_index in children:
@@ -154,6 +194,7 @@ class BinaryRelation():
         return (rels, preds)
 
 
+    # Extract information on relations and predicates, to be used in constructing binary relations
     def rels_and_preds(self, rels, preds, e, child_index, child_node):
         # Get arcs pointing from the entity node (outgoing)
         if child_index in e:
@@ -171,17 +212,31 @@ class BinaryRelation():
             preds[child_node['head']]['compound'].append(child_index)
         return(rels, preds)
 
-    
-if __name__ == "__main__":
-    # execute only if run as a script    
 
+    # Write the binary relations to file
+    def write_to_file(self, r):
+        outdir = self.config.get('Output','out_dir')
+        filename = self.home + '/' + outdir + '/binary_relations.txt'
+        sent_list = sorted(r.keys())
+        with codecs.open(filename, 'a', 'utf8') as f:
+            for sent_no in sent_list:
+                s = 'line: ' + r[sent_no]['sentence'] + '\n'
+                for rel in r[sent_no]['relations']:
+                    s += rel + '\n'
+                s += '\n'
+                f.write(s)
+
+
+if __name__ == "__main__":
+    # Execute only if run as a script
+    
     configfile = 'config.ini'
     cfg = ConfigParser.ConfigParser()
     cfg.read(configfile)
     bin_rel = BinaryRelation(cfg)
     
-    dtree = bin_rel.read_dependency_parse('test.conllu')
+    dtrees = bin_rel.read_dependency_parse('test.conllu')
     
     test_entities = {"file": "none", "sentences": {0: {"entities": {0: {"start": 20, "disambiguatedURL": "http://de.dbpedia.org/resource/Deutsche_Demokratische_Republik", "namedEntity": "DDR", "FIGERType": "/location/country", "offset": 3}, 1: {"start": 0, "disambiguatedURL": "http://de.dbpedia.org/resource/Angela_Merkel", "namedEntity": "Merkel", "FIGERType": "/person/politician", "offset": 6}}, "sentenceStr": "<entity>Merkel</entity> wuchs in der <entity>DDR</entity> auf und war dort als Physikerin wissenschaftlich tätig ."}, 1: {"entities": {0: {"start": 0, "disambiguatedURL": "http://de.dbpedia.org/resource/David_Bowie", "namedEntity": "David Bowie", "FIGERType": "/person", "offset": 11}, 1: {"start": 20, "disambiguatedURL": "null", "namedEntity": "britischer", "FIGERType": "none", "offset": 10}}, "sentenceStr": "<entity>David Bowie</entity> war ein <entity>britischer</entity> Musiker , Sänger , Produzent und Schauspieler ."}}}
 
-    bin_rel.extract(dtree,test_entities)
+    bin_rel.extract(dtrees, test_entities)
