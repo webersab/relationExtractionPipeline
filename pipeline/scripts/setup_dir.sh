152ea29331473a8301aece83f34d680d5caefc16
Liane Guillou <lguillou@darkstar.inf.ed.ac.uk>
2018-04-09 16:53:18 +0100

    Added directory setup script

---
+++ pipeline/scripts/setup_dir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p 00-json-input;
+mkdir -p 01-raw-input;
+mkdir -p 02-segmented-input;
+mkdir -p 03-udpipe-output;
+mkdir -p 04-ner-pre-proc-output;
+mkdir -p 05-ner-output;
+mkdir -p 06-unstable-parser-output;
+mkdir -p 07-unstable-parser-post-proc-output;
+mkdir -p 08-entities-output;
+mkdir -p 09-agdistis-output;
+mkdir -p 10-binary-relations;
