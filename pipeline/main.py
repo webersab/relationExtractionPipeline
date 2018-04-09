c7f07b24db7744d74e51d8f981a1eed578956575
Liane Guillou <lguillou@darkstar.inf.ed.ac.uk>
2018-04-09 17:19:54 +0100

    Uncommented pipeline steps

--- pipeline/main.py
+++ pipeline/main.py
@@ -66,27 +66,28 @@ if __name__ == "__main__":
     configmap = get_config(configfile)
     # Get list of files for processing
     file_list = read_json_input(configmap)
+    file_list = file_list[0:10]
     #file_list = get_file_list(configmap)
     # Sentence segmentation
     preprocessor = pre.Preprocessor(configmap)
     file_list = preprocessor.split_sentences(file_list)
     # Pre-process files with UDPipe
-#    preprocessor.udpipe(file_list)
+    preprocessor.udpipe(file_list)
     # Simultaneously parse and perform NER
-#    parser = parsing.Parser(configmap)
-#    parse_proc = Process(target=parser.parse, args=(file_list,))
-#    parse_proc.start()
+    parser = parsing.Parser(configmap)
+    parse_proc = Process(target=parser.parse, args=(file_list,))
+    parse_proc.start()
     ne_rec = ner.Ner(configmap)
     ner_proc = Process(target=ne_rec.NER, args=(file_list,))
     ner_proc.start()
     # Wait until both processes have completed before continuing
-#    parse_proc.join()
-#    ner_proc.join()
+    parse_proc.join()
+    ner_proc.join()
     # Named Entity Linking
-#    n_link = nel.Nel(configmap)
-#    n_link.NEL(file_list)
+    n_link = nel.Nel(configmap)
+    n_link.NEL(file_list)
     # Extract binary relations
-#    bin_rel = binary_relation.BinaryRelation(configmap)
-#    bin_rel.extract_binary_relations(file_list)
-#    # Exit
+    bin_rel = binary_relation.BinaryRelation(configmap)
+    bin_rel.extract_binary_relations(file_list)
+    # Exit
     logging.info('Finished at: '+str(datetime.now())+'\n\n')
