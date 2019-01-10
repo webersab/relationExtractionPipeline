f3cf3762b4182508f53bd6e51b711b5c768e3009
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-01-10 15:58:04 +0000

    forgot that one lien in main that says nel is a boolean

--- de_pipeline/main.py
+++ de_pipeline/main.py
@@ -92,7 +92,7 @@ def get_pipeline_steps(config):
     # Run batching and relation extraction steps?
     batching = True if start_step == 1 else False
     rel_extraction = True if end_step == 6 else False
-    nel = True if start_step<=4 and end_step>4 else False
+    #nel = True if start_step<=4 and end_step>4 else False
     # Parallel pipeline steps, removed parsing.UnstParser(configmap) from list, 
     parallel_step_list = [pre.Preprocessor(configmap), ner.Ner(configmap),nel.Nel(configmap)]
     parallel_steps = parallel_step_list[max(0,start_step-2):end_step-1]
