b0c458ea6912f32214964ed8fcab9544e129a62d
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-03-26 10:51:23 +0000

    testing multiprocesing trough gnu paralell

---
+++ de_pipeline/skriptOnlyBinRel.py
@@ -0,0 +1,41 @@
+# Standard
+import sys
+import glob
+import logging
+import ConfigParser
+import traceback
+import multiprocessing as mp
+from datetime import datetime
+from functools import partial
+from itertools import chain
+
+# Custom
+import preprocessing as pre
+import helper_functions as hf
+import ner
+import nel
+import binary_relation_withLight
+import simpleTyping
+import copy_reg
+import types
+
+
+
+if __name__ == "__main__":
+    """
+    Calls each of the pipeline steps in sequence, or a partial set of
+    steps if this is specified in the config file
+    """
+    print("HUPHUP")
+    # Set up logging
+    logging.basicConfig(filename='pipeline.log',level=logging.DEBUG)
+    logging.info('Started at: '+str(datetime.now()))
+    # Get command line arguments
+    configfile = sys.argv[1]
+    # Get configuration settings
+    cfg = ConfigParser.ConfigParser()
+    configmap = cfg.read("config.ini")
+    file = sys.argv[1]
+    bin_rel = binary_relation_withLight.BinaryRelationWithLight(configmap)
+    bin_rel.process(file)
+    
